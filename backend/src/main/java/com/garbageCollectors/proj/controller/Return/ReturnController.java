a5e63ee7236625ce470ba9e28ed12a4ee71d8a8f
Ryan Sajan <ryansajan15@gmail.com>
2025-11-21 16:30:41 +0530

    Deleting return package from db

--- backend/src/main/java/com/garbageCollectors/proj/controller/Return/ReturnController.java
+++ backend/src/main/java/com/garbageCollectors/proj/controller/Return/ReturnController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -31,6 +28,14 @@ public class ReturnController {
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteReturnByEmail(@RequestParam String email){
+        if(this.returnRepo.findByEmail(email)==null){
+            return ResponseEntity.notFound().build();
+        }
+        this.returnRepo.deleteByEmail(email);
+        return ResponseEntity.noContent().build();
 
+    }
     
 }
