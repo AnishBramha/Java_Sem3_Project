8e3e104d4a15064ac48b63396d7c4adf09d39df2
Ryan Sajan <ryansajan15@gmail.com>
2025-11-21 10:36:30 +0530

    Pushing return transfer object

--- backend/src/main/java/com/garbageCollectors/proj/controller/Return/ReturnRequestDTO.java
+++ backend/src/main/java/com/garbageCollectors/proj/controller/Return/ReturnRequestDTO.java
@@ -3,9 +3,15 @@ package com.garbageCollectors.proj.controller.Return;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Data
 @Builder
 public class ReturnRequestDTO {
-
-
+    private String email;
+    private LocalDateTime timestamp;
+    private String deliveryCompany;
+    private String name;
+    private List<String> phoneNumbers;
 }
--- backend/src/main/java/com/garbageCollectors/proj/controller/Return/ReturnResponseDTO.java
+++ backend/src/main/java/com/garbageCollectors/proj/controller/Return/ReturnResponseDTO.java
@@ -3,9 +3,16 @@ package com.garbageCollectors.proj.controller.Return;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Data
 @Builder
 public class ReturnResponseDTO {
-
-
+    private String id;
+    private String email;
+    private LocalDateTime timestamp;
+    private String deliveryCompany;
+    private String name;
+    private List<String> phoneNumbers;
 }
