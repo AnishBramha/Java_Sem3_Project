ace1dc9939ead19acf23371954c689d6cf04ad00
HarshaVPolar <hvr9849560299@gmail.com>
2025-11-26 16:16:49 +0530

    Update PackageRepo.java

--- backend/src/main/java/com/garbageCollectors/proj/model/Package/PackageRepo.java
+++ backend/src/main/java/com/garbageCollectors/proj/model/Package/PackageRepo.java
@@ -1,10 +1,13 @@
 package com.garbageCollectors.proj.model.Package;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PackageRepo extends MongoRepository<Package, String> {
-
-
+    List<Package> findByStatus(String status, Pageable pageable);
+    List<Package> findByPhoneNumberAndStatus(String status, String phoneNumber);
+    List<Package> findByPhoneNumber(String phoneNumber);
 }
